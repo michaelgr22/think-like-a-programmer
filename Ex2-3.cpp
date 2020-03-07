38b3a628a3199a64d8b005e10feeafba44c0fc69
michaelgr22 <michael.graml22@outlook.de>
2020-03-07 13:52:26 +0100

    added Excercise 2-3

---
+++ Ex2-3.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+using std::cin;
+using std::cout;
+
+int main()
+{
+    for (int row = 0; row < 8; row++)
+    {
+        if (row < 8 / 2)
+        {
+            for (int c = 0; c < row; c++)
+            {
+                cout << " ";
+            }
+            for (int c = 0; c < row + 1; c++)
+            {
+                cout << "#";
+            }
+            for (int c = 0; c < 14 - ((row + 1 + row) * 2); c++)
+            {
+                cout << " ";
+            }
+            for (int c = 0; c < row + 1; c++)
+            {
+                cout << "#";
+            }
+            for (int c = 0; c <= row; c++)
+            {
+                cout << " ";
+            }
+        }
+        else
+        {
+            for (int c = 0; c < 7 - row; c++)
+            {
+                cout << " ";
+            }
+            for (int c = 0; c < 8 - row; c++)
+            {
+                cout << "#";
+            }
+            for (int c = 0; c < (row - (8 - row)) * 2; c++)
+            {
+                cout << " ";
+            }
+            for (int c = 0; c < 8 - row; c++)
+            {
+                cout << "#";
+            }
+            for (int c = 0; c < 7 - row; c++)
+            {
+                cout << " ";
+            }
+
+        }
+        cout << "\n";
+    }
+    
+}
+
+
+
